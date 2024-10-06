3f1894e28bdb62a65fce1ad97c0c4ab934a33076
IevgeniiGlagoliev <glagolev.dev@gmail.com>
2024-10-07 00:52:09 +0300

    finish second  page layout

--- js/main.js
+++ js/main.js
@@ -3,15 +3,7 @@ const embedEngine = {
         embedEngine.binds();
     },
     binds() {
-        const swiper = new Swiper('.swiper', {
-            slidesPerView: 1,
-            calculateHeight:true,
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        });
+
     },
 };
 document.addEventListener("DOMContentLoaded", embedEngine.init);
